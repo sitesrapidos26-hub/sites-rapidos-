dead8d6f6ec51fb8b93f702a47ccd32743b94f96
sitesrapidos26-hub <sites.rapidos26@gmail.com>
2026-03-12 15:31:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll("section")
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity = "1"
+entry.target.style.transform = "translateY(0)"
+
+}
+
+})
+
+})
+
+sections.forEach(section => {
+
+section.style.opacity = "0"
+section.style.transform = "translateY(40px)"
+section.style.transition = "all 0.7s ease"
+
+observer.observe(section)
+
+})
