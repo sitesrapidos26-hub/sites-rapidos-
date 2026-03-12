94c1a9d5e402ec21ad1d6144621c97f0b179630c
sitesrapidos26-hub <sites.rapidos26@gmail.com>
2026-03-12 15:16:39 -0300

    Add files via upload

---
+++ barbearia/carousel.js
@@ -0,0 +1,66 @@
+const track = document.querySelector(".carousel-track")
+const slides = document.querySelectorAll(".carousel-track img")
+const next = document.querySelector(".next")
+const prev = document.querySelector(".prev")
+
+let index = 0
+let autoplay
+let restartTimer
+
+let slideWidth
+
+window.addEventListener("load",()=>{
+slideWidth = slides[0].offsetWidth + 20
+startAuto()
+})
+
+const visibleSlides = window.innerWidth < 768 ? 1 : 3
+const maxIndex = slides.length - visibleSlides
+
+function updateCarousel(){
+track.style.transform = `translateX(-${index * slideWidth}px)`
+}
+
+function nextSlide(){
+index++
+if(index > maxIndex){
+index = 0
+}
+updateCarousel()
+}
+
+function prevSlide(){
+index--
+if(index < 0){
+index = maxIndex
+}
+updateCarousel()
+}
+
+function startAuto(){
+autoplay = setInterval(nextSlide, 3000)
+}
+
+function pauseAuto(){
+clearInterval(autoplay)
+
+clearTimeout(restartTimer)
+
+restartTimer = setTimeout(()=>{
+startAuto()
+},5000)
+}
+
+next.addEventListener("click", ()=>{
+nextSlide()
+pauseAuto()
+})
+
+prev.addEventListener("click", ()=>{
+prevSlide()
+pauseAuto()
+})
+
+window.addEventListener("load",()=>{
+startAuto()
+})
---
+++ barbearia/script.js
@@ -0,0 +1,43 @@
+// animações ao aparecer
+const observer=new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show")
+}
+})
+},{threshold:.2})
+
+document.querySelectorAll(".fade").forEach(el=>{
+observer.observe(el)
+})
+
+
+// MENU MOBILE
+
+const menuBtn=document.getElementById("menu-btn")
+const menu=document.getElementById("menu")
+
+menuBtn.addEventListener("click",()=>{
+menu.classList.toggle("show")
+})
+
+
+// SCROLL SUAVE
+
+const links=document.querySelectorAll("nav a")
+
+links.forEach(link=>{
+link.addEventListener("click",e=>{
+
+e.preventDefault()
+
+const target=document.querySelector(link.getAttribute("href"))
+
+target.scrollIntoView({
+behavior:"smooth"
+})
+
+menu.classList.remove("show")
+
+})
+})
